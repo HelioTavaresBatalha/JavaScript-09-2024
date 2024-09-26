edf95b1bbb03bb3448b917521ccd5875adc53809
Helio Batalha <helio_batalha@icloud.com>
2024-09-26 17:30:56 +0100

    Praticando funções em Js

---
+++ funcoes-js/ex001.js
@@ -0,0 +1,10 @@
+let num = 0;
+function calcular() {
+
+    if (num%2==0){
+        console.log('O número é par');
+    } else {
+        console.log('O número é impar');
+    }
+}
+calcular(20);
---
+++ funcoes-js/ex002.js
@@ -0,0 +1,7 @@
+function calcularAreaTerreno(largura,comprimento){
+    var area = largura * comprimento;
+
+    return area;
+}
+var area = calcularAreaTerreno(15,25);
+console.log( 'O terreno possui ' + area + ' metros quadrados') ;
